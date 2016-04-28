8373a72171500cb83d9b2017c6699ca5240fbe1c
1513911 <1513911@rgu.ac.uk>
2016-04-28 16:03:28 +0100

    implementing parameters

--- PhotoComment/login.php
+++ PhotoComment/login.php
@@ -3,37 +3,53 @@
 	include("connection.php"); //Establishing connection with our database
 	
 	$error = ""; //Variable for storing our errors.
-	if(isset($_POST["submit"]))
-	{
-		if(empty($_POST["username"]) || empty($_POST["password"]))
-		{
+	if(isset($_POST["submit"])) {
+		if (empty($_POST["username"]) || empty($_POST["password"])) {
 			$error = "Both fields are required.";
-		}else
-		{
+		} else {
 			// Define $username and $password
-			$username=$_POST['username'];
-			$password=$_POST['password'];
+			$username = $_POST['username'];
+			$password = $_POST['password'];
 
 
-			
-			//Check username and password from database
-			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql);
-			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
-			
-			//If username and password exist in our database then create a session.
-			//Otherwise echo error.
-			
-			if(mysqli_num_rows($result) == 1)
-			{
-				$_SESSION['username'] = $username; // Initializing Session
-				header("location: photos.php"); // Redirecting To Other Page
-			}else
-			{
-				$error = "Incorrect username or password.";
+			//define sql connection
+			$sqliconn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+			$photosql = 'SELECT userID FROM users WHERE username=? and password= ?';
+
+			//inititalilised the statement
+			$stm = $sqlidb->init();
+
+			//prepare statement
+			if (!($stm->prepare($photosql))) {
+				echo "prepared statement failed";
+
+				//bind parameter
+				$stm->bind_param('ss', $username, $password);
 			}
+				if($stm->execute()) {
+					$result = $stm->get_result();
+					$row = $result->fetch_assoc();
+
+
+					//Check username and password from database
+					//	$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+					//$result = mysqli_query($db, $sql);
+					//$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+					//If username and password exist in our database then create a session.
+					//Otherwise echo error.
+
+					//if (mysqli_num_rows($result) == 1) {
+					$_SESSION['username'] = $username; // Initializing Session
+					header("location: photos.php"); // Redirecting To Other Page
+
+				}
+				 else {
+					$error = "Incorrect username or password.";
+				}
+
 
 		}
 	}
-
 ?>
