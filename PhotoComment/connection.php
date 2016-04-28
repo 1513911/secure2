f6e7b0a925df8c3b38711b388578c257a31a1f25
1513911 <1513911@rgu.ac.uk>
2016-04-28 17:31:02 +0100

    implementing parameters

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -4,5 +4,5 @@ define('DB_USERNAME', 'b388f653be8ad5');
 define('DB_PASSWORD', 'a441d375');
 define('DB_DATABASE', 'weblab2');
 
-
+$db=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
--- PhotoComment/login.php
+++ PhotoComment/login.php
@@ -3,10 +3,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
+include("connection.php"); //Establishing connection with our database
+
+
 ?>
 <?php
-include("connection.php"); //Establishing connection with our database
 
 function get_client_ip() {
 	$ipaddress = '';
