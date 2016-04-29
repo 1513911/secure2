0a6ad7b16e0600ad1f51327837eb4e763464a7fa
1513911 <1513911@rgu.ac.uk>
2016-04-29 23:33:07 +0100

    Update addcomment.php

--- PhotoComment/addcomment.php
+++ PhotoComment/addcomment.php
@@ -6,17 +6,43 @@ $msg = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
 {
 
+
     $desc = $_POST["desc"];
     $photoID = $_POST["photoID"];
     $name = $_SESSION["username"];
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) == 1) {
+    //clean input description
+    $desc = stripslashes( $desc );
+    $desc=mysqli_real_escape_string($db,$desc);
+    $desc = htmlspecialchars($desc);
+
+    //clean input photo id
+    $photoID = stripslashes( $photoID );
+    $photoID=mysqli_real_escape_string($db,$photoID);
+    $photoID = htmlspecialchars($photoID);
+
+    //declare instance of connection
+    $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+    if (!($sqlcon->connect_errno)){
+        echo"connection Failed";
+    }
+    //prepare statem
+    if($stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=?")){
+        //bind parameter
+        $stmt->bind_param('s',$name);
+        $stmt->execute();
+        //get result
+        $result = $stmt->get_result();
+    }
+
+    if( ($row=$result->fetch_row()))
+    { 
+        
+        {
         //echo $name." ".$email." ".$password;
-        $id = $row['userID'];
-        $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
+        $id = $row[0];
+        $addsql = "INSERT INTO commentsSecure (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
         $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
         if ($query) {
             $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
