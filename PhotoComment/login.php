e50b684da41c3b7c0ba3aa9a06cd30ac4014ade6
1513911 <1513911@rgu.ac.uk>
2016-04-29 22:24:59 +0100

    Update login.php

--- PhotoComment/login.php
+++ PhotoComment/login.php
@@ -3,42 +3,50 @@
 session_start();
 ?>
 <?php
-	include("connection.php"); //Establishing connection with our database
-	
-	$error = ""; //Variable for storing our errors.
-	if(isset($_POST["submit"]))
+include("connection.php"); //Establishing connection with our database
+
+$error = ""; //Variable for storing our errors.
+if(isset($_POST["submit"]))
+{
+	if(empty($_POST["username"]) || empty($_POST["password"]))
 	{
-		if(empty($_POST["username"]) || empty($_POST["password"]))
+		$error = "Both fields are required.";
+	}else
+	{
+		// Define $username and $password
+		$username=$_POST['username'];
+		$password=$_POST['password'];
+
+
+		//implement prepared statement to take of sql injection and other vulnerabilities
+
+		//declare instance of connection
+		$sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if (!($sqlcon->connect_errno)){
+			echo"connection Failed";
+		}
+
+		//prepare statement
+		if($stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=? and password=?")){
+			//bind parameter
+			$stmt->bind_param('ss',$_POST["username"],$_POST["password"]);
+			$stmt->execute();
+			//get result
+			$result = $stmt->get_result();
+		}
+
+
+		if( ($row=$result->fetch_row()))
 		{
-			$error = "Both fields are required.";
+			$_SESSION['username'] = $username; // Initializing Session
+			$_SESSION["userid"] = $userid;//user id assigned to session global variable
+			header("location: photos.php"); // Redirecting To Other Page
 		}else
 		{
-			// Define $username and $password
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-
-
-			
-			//Check username and password from database
-			$sql="SELECT userID FROM userssecure WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql);
-			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
-			$userid=$row['userID'];//Get user ID
-			
-			//If username and password exist in our database then create a session.
-			//Otherwise echo error.
-			
-			if(mysqli_num_rows($result) == 1)
-			{
-				$_SESSION['username'] = $username; // Initializing Session
-				$_SESSION["userid"] = $userid;//user id assigned to session global variable
-				header("location: photos.php"); // Redirecting To Other Page
-			}else
-			{
-				$error = "Incorrect username or password.";
-			}
-
+			$error = "Incorrect username or password.";
 		}
+
 	}
+}
 
 ?>
