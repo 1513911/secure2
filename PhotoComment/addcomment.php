b9b0b3df2bff6497a4aba9f1df2f429eb6a87f4c
1513911 <1513911@rgu.ac.uk>
2016-04-29 23:52:52 +0100

    Update addcomment.php

--- PhotoComment/addcomment.php
+++ PhotoComment/addcomment.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 include("connection.php"); //Establishing connection with our database
+//declare instance of connection
+$sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (!($sqlcon->connect_errno)){
+    echo"connection Failed";
+}
 
 $msg = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
@@ -10,12 +15,23 @@ if(isset($_POST["submit"]))
     $photoID = $_POST["photoID"];
     $name = $_SESSION["username"];
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) == 1) {
+
+    //prepare statem
+    if($stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=?")){
+        //bind parameter
+        $stmt->bind_param('s',$_POST["username"]);
+        $stmt->execute();
+        //get result
+        $result = $stmt->get_result();
+    }
+
+   // $sql="SELECT userID FROM users WHERE username='$name'";
+   // $result=mysqli_query($db,$sql);
+    //$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    //if(mysqli_num_rows($result) == 1)
+    if( ($row=$result->fetch_row())) {
         //echo $name." ".$email." ".$password;
-        $id = $row['userID'];
+        $id = $row[0];
         $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
         $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
         if ($query) {
