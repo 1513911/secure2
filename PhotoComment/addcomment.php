7afd1d20943e05225713eb982b0078d64c897a4c
1513911 <1513911@rgu.ac.uk>
2016-04-30 01:09:43 +0100

    Update addcomment.php

--- PhotoComment/addcomment.php
+++ PhotoComment/addcomment.php
@@ -6,16 +6,12 @@ $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if (!($sqlcon->connect_errno)){
     echo"connection Failed";
 }
-
 $msg = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
 {
-
     $desc = $_POST["desc"];
     $photoID = $_POST["photoID"];
     $name = $_SESSION["username"];
-
-
     //prepare statem
     if($stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=?")){
         //bind parameter
@@ -24,23 +20,30 @@ if(isset($_POST["submit"]))
         //get result
         $result = $stmt->get_result();
     }
-
-   // $sql="SELECT userID FROM users WHERE username='$name'";
-   // $result=mysqli_query($db,$sql);
+    // $sql="SELECT userID FROM users WHERE username='$name'";
+    // $result=mysqli_query($db,$sql);
     //$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
     //if(mysqli_num_rows($result) == 1)
     if( ($row=$result->fetch_row())) {
         //echo $name." ".$email." ".$password;
         $id = $row[0];
-        $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
-        $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
-        if ($query) {
-            $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
-        }
+        
     }
-    else{
+    else
+    {
         $msg = "You need to login first";
     }
+    //prepare statement
+    //$stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=?");
+    $stmt=$sqlcon->prepare("INSERT INTO comments (description,photoID,userID) VALUES (?,?,?)");
+    //bind parameter
+    $stmt->bind_param('sii',$desc,$photoID, $id);
+    //get result
+    //$query = mysqli_query($db, $addsql) or die(mysqli_error($db));
+    if ($stmt->execute());
+    {
+        $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
+        
+    }
 }
-
 ?>
