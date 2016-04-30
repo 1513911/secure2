b6f75ab7642de77a68d1b2e5c73e28536c793d05
1513911 <1513911@rgu.ac.uk>
2016-04-30 12:19:05 +0100

    Update addcomment.php

--- PhotoComment/addcomment.php
+++ PhotoComment/addcomment.php
@@ -1,49 +1,68 @@
 <?php
 session_start();
 include("connection.php"); //Establishing connection with our database
-//declare instance of connection
-$sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-if (!($sqlcon->connect_errno)){
-    echo"connection Failed";
-}
+?>
+
+
+<?php
+//connect to db
+$mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if(!$mysqli) die('Could not connect$: ' . mysqli_error());
+
+//get the session variables
+
+$name = $_SESSION["username"];
+$userID=$_SESSION["userid"];
+?>
+<?php
 $msg = ""; //Variable for storing our errors.
+
 if(isset($_POST["submit"]))
 {
+    // Check Anti-CSRF token
+    // checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+
+
     $desc = $_POST["desc"];
     $photoID = $_POST["photoID"];
     $name = $_SESSION["username"];
-    //prepare statem
-    if($stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=?")){
-        //bind parameter
-        $stmt->bind_param('s',$_POST["username"]);
-        $stmt->execute();
-        //get result
-        $result = $stmt->get_result();
+
+    //clean input description
+    $desc = stripslashes( $desc );
+    $desc=mysqli_real_escape_string($db,$desc);
+    $desc = htmlspecialchars( $desc );
+    $desc=xssafe($desc);
+
+    //clean input name
+    $name = stripslashes( $name );
+    $name=mysqli_real_escape_string($db,$name);
+    $name = htmlspecialchars($name);
+    $name=xssafe($name);
+
+    //clean input photo ID
+    $photoID = stripslashes( $photoID );
+    $photoID=mysqli_real_escape_string($db,$photoID);
+    $photoID = htmlspecialchars($photoID);
+    $photoID=xssafe($photoID);
+
+    if($userID >0) {
+        //test connection
+        if ($mysqli->connect_errno) {
+            echo "Connetion Failed:check network connection";// to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+
+        //call procedure
+        if (! $mysqli->query("CALL addComments('$desc','$photoID','$userID')"))  {
+            echo "Procedure Call Failed:";
+
+        }else{
+
+            $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
+        }
     }
-    // $sql="SELECT userID FROM users WHERE username='$name'";
-    // $result=mysqli_query($db,$sql);
-    //$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    //if(mysqli_num_rows($result) == 1)
-    if( ($row=$result->fetch_row())) {
-        //echo $name." ".$email." ".$password;
-        $id = $row[0];
-        
-    }
-    else
-    {
+    else{
         $msg = "You need to login first";
     }
-    //prepare statement
-    //$stmt=$sqlcon->prepare("SELECT userID FROM users WHERE username=?");
-    $stmt=$sqlcon->prepare("INSERT INTO comments (description,photoID,userID) VALUES (?,?,?)");
-    //bind parameter
-    $stmt->bind_param('sii',$desc,$photoID, $id);
-    //get result
-    //$query = mysqli_query($db, $addsql) or die(mysqli_error($db));
-    if ($stmt->execute());
-    {
-        $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
-        
-    }
 }
+
 ?>
